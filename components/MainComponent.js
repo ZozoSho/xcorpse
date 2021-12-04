f05d791f121ad7ddc498e7034bf0c761d3a1a091
Zoe <sponsler.hoehn@gmail.com>
2021-12-04 02:15:16 -0600

    older expo version

---
+++ components/MainComponent.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+import Play from './PlayComponent';
+
+class Main extends Component {
+    constructor(props) {
+        super(props);
+    }
+
+    render() {
+        return <Play />;
+    }
+}
+
+export default Main;
